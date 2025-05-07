d345451ae3c2e59a48c303267c4a0a6fd816143f
ArmandoCasas14 <luis807.555@gmail.com>
2025-05-07 17:47:41 -0500

    prueba

---
+++ Controllers/PruebaController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_Microservicios_Asp_Net.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PruebaController : ControllerBase
+    {
+    }
+}
