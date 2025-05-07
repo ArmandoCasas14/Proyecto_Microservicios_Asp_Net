19cbaa610de1d50516c2686351102baba323a5cb
ArmandoCasas14 <luis807.555@gmail.com>
2025-05-07 18:23:37 -0500

    prueba2

--- Controllers/PruebaController.cs
+++ Controllers/PruebaController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Proyecto_Microservicios_Asp_Net.Controllers
 {
+    // esto es una prueba de commit
     [Route("api/[controller]")]
     [ApiController]
     public class PruebaController : ControllerBase
